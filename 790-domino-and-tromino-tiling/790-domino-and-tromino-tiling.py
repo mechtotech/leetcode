cc600e9e1384c39ea6d38e999cd259d7ca669d1c
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-02-17 11:45:27 -0500

    Time: 77 ms (17.50%), Space: 14.2 MB (60.07%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -1,49 +1,45 @@
 class Solution:
     def numTilings(self, n: int) -> int:
         '''
-        2 xn tiles f(n)
-        last tile can covered with 1 tile | f(n-1)   =  f(n-2) 
-        or _|   =  L(n-2) = |||||  ||||-  L(n) = f(n-1) + U(n-1)
-        L(n-2) = f(n-3) + U(n-3)
-        |-  = U(n-2) ||||..|_ U(n) = f(n-1)+ L(n-2
+        f(n) = number of ways to tile 2 xn board
+        f(n-1) = number of ways to tile 2 x(n-1) board
         
-        f(n) = f(n-1)+L(n-2)+U(n-2)+f(n-2)
-        f(1) = 1
-        f(2) = 2
-        f(3)  =5
+        x    xx
+        x    xx   
         
-        f(1)--> f(2)
-        l(1)---> l(2)
-        u(1)---> u(2)
-    
+        xx   x
+        x   xx
         
+        u(n) = -------
+               ------
+        f(n) = f(n-1) + u(n-2) +l(n-2) +f(n-2)
         
+        u(n) = f(n-1) + l(n-1)
+        l(n) = f(n-1) +u(n-1)
         
         '''
-        
-        DP=[None]*3
-        L=[None]*3
-        U = [None]*3
-      
-        
-        if n <=2:
+        if n ==1 or n==2:
             return n
         
-        else:
-            DP[1] = 1
-            DP[2] = 2
-            L[1] =1
-            U[1] = 1
-            L[2] = 2
-            U[2] = 2
+        F = [None]*(n+1)
+        L= [None]*(n+1)
+        U = [None]*(n+1)
+        
+        F[1]= 1
+        F[2] = 2
+        
+        
+        L[1] = 1
+        L[2] = 2
+        U[1] =1
+        U[2] =2
+        
+        for i in range(3,n+1):
+            F[i] = F[i-1]+F[i-2]+L[i-2]+U[i-2]
+            L[i] = F[i-1] + L[i-1]
+            U[i] =F[i-1] +U[i-1]
+        return F[n]%(10**9+7)
             
-            
-            for i in range(3,n+1):
-                DP[i%3] = (DP[(i-1)%3]+L[(i-2)%3]+U[(i-2)%3]+DP[(i-2)%3])
-                L[i%3] = (DP[(i-1)%3]+ U[(i-1)%3])
-                U[i%3] = (DP[(i-1)%3] + L[(i-1)%3])
-                
-            return DP[n%3]%((10**9)+7)
                 
         
         
