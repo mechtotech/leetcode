68e8ab8eaed501814a18ecb529bad372a795d83c
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-02-20 15:48:29 -0500

    Time: 62 ms (13.16%), Space: 14 MB (71.49%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,51 +1,50 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         '''
-        robber 
         
-        circular arrangement needs to converted to linear arrangement
+        f(n) = maximum amount you can rob from n-1 houses
         
-        0...n-1 houses
+        f(n) = max of f(n-1) or f(n-2)+nums[n]
         
-        case 1) if 0 house gets robbed 
-        
-        then our linear arrangement becomes 2...n-2
-        
-        case 2) 0...n-1 incase first house is not robbed
+        case1 when house 0 is  robbed
+        0 & n-1 next to eachother      0 1 2 3 4 
+        nums[2.....n-2]
         
         
         
+        case 2 when house 0 is not robbed
+        1....n-1
         '''
-        if len(nums)==0:
+        
+        n= len(nums)
+        
+        if n == 0:
             return 0
-        if len(nums)==1:
+        if n == 1:
             return nums[0]
-        if len(nums)==2:
-            return max(nums[0], nums[1])
-        if len(nums)==3:
-            return max(nums[0],nums[2],nums[1])
-        if len(nums)==4:
-            return max(nums[0]+nums[2],nums[1]+nums[3])
-        DP = [None for i in range(len(nums))]
+        if n==2:
+            return max(nums[1],nums[0])
+        if n==3:
+            return max(nums[0],nums[1],nums[2])
+        if n==4:
+            return max(nums[0]+nums[2], nums[1]+nums[3])
         
+        DP = [None] * n
+        #case1:
         
-        
-        DP[2]= nums[2]
+        DP[2] = nums[2]
         DP[3] = max(nums[2],nums[3])
+        for i in range(4,n):
+            DP[i] = max(DP[i-1],DP[i-2]+nums[i])
         
-        for i in range (4,len(nums)-1):
-            DP[i]=max(nums[i]+DP[i-2],DP[i-1])
-        
-        case1= nums[0]+DP[len(nums)-2]
-            
-            
-       
+        case1 = DP[n-2] +nums[0]
+        #case2:
         DP[1] = nums[1]
-        DP[2]= max(nums[1],nums[2])
+        DP[2] = max(nums[1],nums[2])
+        for i in range(3,n):
+            DP[i] = max(DP[i-1],DP[i-2]+nums[i])
+        case2 = DP[n-1]
         
-        for i in range(3,len(nums)):
-            DP[i] = max(nums[i]+DP[i-2],DP[i-1])
-        case2 = DP[len(nums)-1]
         
         return max(case1,case2)
         
@@ -53,11 +52,8 @@ class Solution:
         
         
         
+            
         
-        
-        
-        
-        
-        
+    
         
         
