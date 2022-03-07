ac16401f4fe7c20661de13f08f5c98ee2f9deb3e
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-03-06 19:49:00 -0500

    Time: 1015 ms (40.16%), Space: 21.3 MB (91.00%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,13 +1,39 @@
-import heapq
+import random
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        result = []
+        def helper(a,start,end):
+            
+            
+            if start>= end:
+                return
+            
+            pivot = random.randint(start,end)
+            
+            a[pivot], a[start] = a[start],a[pivot]
+            
+            orange = start
+            
+            for green in range(start+1,end+1):
+                
+                if a[green] < a[start]:
+                    orange+=1
+                    a[green],a[orange] = a[orange],a[green]
+            
+            a[orange],a[start] = a[start],a[orange]
+            
+            helper(a,start,orange-1)
+            helper(a,orange+1,end)
         
-        heapq.heapify(nums)
+        helper(nums,0,len(nums)-1)
+        return nums
+            
+            
+            
         
-        while len(nums)!=0:
-            result.append(heapq.heappop(nums))
         
-        return result
+        
+        
+        
+    
         
