79e384e9d1e0f8a492db27dc84bd8a24b5553c37
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-03-19 16:12:09 -0400

    Time: 67 ms (22.11%), Space: 14 MB (17.18%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,36 +6,36 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        sentinel = ListNode()
+        sentinel = ListNode("dummy",None)
         tail = sentinel
-        
-        # take out the minimum of head of l1 or l2
-        # create a new linked list which is sorted
-        l1 =list1
-        l2 = list2
+        l1 = list1
+        l2 =list2
         while l1 and l2:
-            if l1.val <=l2.val:
+            if l1.val <= l2.val:
                 tail.next = l1
                 tail = l1
+               
                 l1 = l1.next
-                tail.next = None
             else:
                 tail.next = l2
                 tail = l2
-                l2= l2.next
-                tail.next = None
+                
+                l2 = l2.next
+        
         while l1:
             tail.next = l1
             tail = l1
+            
             l1 = l1.next
-            tail.next = None
         while l2:
             tail.next = l2
             tail = l2
-            l2= l2.next
-            tail.next = None
-        
+            
+            l2 = l2.next
         
         return sentinel.next
             
-                
+            
+        
+        
+        
