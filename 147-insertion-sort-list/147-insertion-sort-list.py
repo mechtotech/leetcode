a792066d7a0e261fca64347e9cec9e0c7a7de914
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-03-20 15:58:25 -0400

    Time: 1495 ms (49.58%), Space: 16.6 MB (76.39%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.py
+++ 147-insertion-sort-list/147-insertion-sort-list.py
@@ -6,28 +6,33 @@
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        sentinel = ListNode("dummy",None)
         
-        sentinel = ListNode()
         
-        curr = head
+        curr  = head
         
         while curr:
-            succ = curr.next
+            #1)extract the node
+            #2) add the node
+            #3) increment the node
+            succ =curr.next
             curr.next = None
             
-            
             prev = sentinel
-            curr2 =sentinel.next
-            
+            curr2 = sentinel.next
             while curr2 and curr.val > curr2.val:
-                prev= curr2
+                prev = curr2
                 curr2 = curr2.next
             
             prev.next = curr
             curr.next = curr2
             
-            
-            curr = succ
         
+            curr = succ
         return sentinel.next
             
+            
+            
+            
+            
+       
