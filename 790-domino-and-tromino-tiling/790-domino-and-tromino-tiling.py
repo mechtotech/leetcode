f0c1f0c7aaf3632f6d953c83fadd12ac3d5e43b2
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-05-05 18:14:04 -0400

    Time: 60 ms (37.87%), Space: 14.3 MB (34.29%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -1,45 +1,49 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        '''
-        f(n) = number of ways to tile 2 xn board
-        f(n-1) = number of ways to tile 2 x(n-1) board
+        """
+        f(n) = no. of ways to tile 2 x n board
         
-        x    xx
-        x    xx   
+        f(n) = U(n-1) + L(n-1) + f(n-1)+f(n-2)
         
-        xx   x
-        x   xx
         
-        u(n) = -------
-               ------
-        f(n) = f(n-1) + u(n-2) +l(n-2) +f(n-2)
+        XX     
+        X  
         
-        u(n) = f(n-1) + l(n-1)
-        l(n) = f(n-1) +u(n-1)
+        f(1) = 1
+        f(2) = 2
+        f(3) = 5
         
-        '''
-        if n ==1 or n==2:
+        
+        
+        """
+        if n == 1 or n==2:
             return n
+        if n == 3:
+            return 5
+        f = [0 for i in range(n+1)]
+        u = [0 for i in range(n+1)]
+        l = [0 for i in range(n+1)]
         
-        F = [None]*(n+1)
-        L= [None]*(n+1)
-        U = [None]*(n+1)
-        
-        F[1]= 1
-        F[2] = 2
+        f[1] = 1
+        f[2] = 2
         
         
-        L[1] = 1
-        L[2] = 2
-        U[1] =1
-        U[2] =2
+        l[1] = 1
+        l[2] = 2
+        
+        u[1] = 1
+        u[2] = 2
         
         for i in range(3,n+1):
-            F[i] = F[i-1]+F[i-2]+L[i-2]+U[i-2]
-            L[i] = F[i-1] + L[i-1]
-            U[i] =F[i-1] +U[i-1]
-        return F[n]%(10**9+7)
-            
+            f[i] = f[i-1]+f[i-2] + l[i-2] + u[i-2]
+            u[i] = f[i-1] + l[i-1]
+            l[i] = f[i-1] +u[i-1]
+           
+        
+        return f[n]%(10**9+7)
+        
+        
+        
                 
         
         
