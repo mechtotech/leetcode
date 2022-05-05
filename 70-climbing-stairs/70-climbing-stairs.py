b21bc5a59606166b590ac9d7186deb2b7f8e79f5
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-05-05 16:32:06 -0400

    Time: 49 ms (28.49%), Space: 13.8 MB (96.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,20 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         '''
-        f(n) # number of distinct to climb n steps
-        f(n) = f(n-1 + f(n-2)
         
+        f(n) == no. of distinct ways to climb n step
+        f(n) = f(n-1) + f(n-2) 
+        
+        DP=[0 for i in range(n+1)]
+        
+        DP[0] = 0
+        DP[1] = 1
+        DP[2]  = 2
         '''
-        
-        if n <=2:
+        if n == 1 or n == 2:
             return n
-        else:
-            DP = [0 for i in range(n+1)]
+        DP=[0 for i in range(n+1)]
+        DP[1] = 1
+        DP[2]  = 2
+        for i in range(3,n+1):
+            DP[i] = DP[i-1]+DP[i-2]
         
-            DP[1] = 1
-            DP[2] = 2
+        return DP[n]
+       
             
-            for i in range(3,n+1):
-                DP[i] = DP[i-1]+DP[i-2]
-            
-            return DP[n]
