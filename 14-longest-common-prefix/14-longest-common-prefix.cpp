c9ee4359e6b74dfe64cb7ea2042cc4cb213a7cf3
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-05-07 16:04:17 -0400

    Time: 0 ms (100.00%), Space: 9.2 MB (44.41%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,23 +1,24 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        strs.sort()
-        
-        string_first = strs[0]
-        string_last = strs[-1]
-        count=0
-        for i in range(len(string_first)):
-            if string_first[i] == string_last[i]:
-                count+=1
-            else:
-                break
-        if count == 0:
-            return ""
-        else:
-            return string_first[0:count]
-                
-        
-        
+#include <vector>
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string string_first = strs[0];
+        string string_last = strs.back();
+        int count = 0;
+        for (int i =0; i < string_first.size(); i++){
+            if (string_first[i] == string_last[i]){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        if (count == 0){
+            return "";
+        }
+        else{
+            return string_first.substr(0, count);
+        }
+    }
+};
