0e80c4ac29c67c6c9eddfa4e69419de67cfe180c
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-05-08 18:34:12 -0400

    Time: 29 ms (88.29%), Space: 13.9 MB (27.97%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -1,40 +1,44 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        #1. split the original string by .
-        version1 = version1.split('.') 
-        version2 = version2.split('.')
+        version1 = version1.split(".")
+        version2 = version2.split(".")
         
-        n1 = len(version1)
+        n1= len(version1)
         n2 = len(version2)
         n = n1
         
-        #2. append '0' to the shorter list if there is one
-        if n1 > n2:
-            version2 += ['0'] * (n1-n2)
-            n = n1
-            
-        if n1 < n2:
-            version1 += ['0'] * (n2-n1)
-            n = n2
-
-
-        #3. use two pointers to walk through 
-        # the two list and compare
-        # one by one position   
-        pt1 = 0
-        pt2 = 0
         
-        while pt1 < n and pt2 < n:
-            if int(version1[pt1]) > int(version2[pt2]):
+        # append zero to smaller version
+        if n1>n2:
+            version2+= ['0']*(n1-n2)
+            n= n1
+        if n2 >n1:
+            version1 +=['0']*(n2-n1)
+            n=n2
+            
+            
+        i = 0
+        j = 0
+        
+        while i < n and j < n:
+            if int(version1[i]) > int(version2[j]):
                 return 1
-            elif int(version1[pt1]) < int(version2[pt2]):
+            elif int(version1[i]) < int(version2[j]):
                 return -1
             else:
-                pt1 += 1
-                pt2 += 1
+                i += 1
+                j += 1
                 
             
         return 0
+        
+        
+        
+    
+       
+    
+            
+        
         
         
         
