1f7b5720302a38ab3a92872be10bbd8c20d2ba94
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-06-25 16:42:28 -0400

    Time: 498 ms (39.19%), Space: 16.5 MB (47.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -24,15 +24,26 @@ class Solution:
                 result.append((row,col+1))
             return result
         
+        
+        
+        def bfs(x,y):
+            visited[x][y] = 1
+            q= deque()
+            q.append((x,y))
+            while len(q) >0:
+                (row,col) = q.popleft()
+                for (r,c) in neighbor(row,col):
+                    if visited[r][c] == -1 and grid[r][c] == '1':
+                        visited[r][c] = 1
+                        q.append((r,c))
         count = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if visited[row][col] == -1 and grid[row][col] == '1':
                     count+=1
-                    dfs(row,col)
+                    bfs(row,col)
         return count
-        
-        #def bfs(x,y):
+                        
             
         
                 
