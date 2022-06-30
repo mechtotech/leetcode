262284bf29af2b561cfa01a990fa9a03eef158f0
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-06-30 00:16:44 -0400

    Time: 98 ms (45.62%), Space: 15.1 MB (68.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -7,9 +7,11 @@ class Solution:
         """
         
         maxprofit = 0
+        
         for i in range(1,len(prices)):
+            
             if prices[i] > prices[i-1]:
-                maxprofit+=prices[i]-prices[i-1]
+                maxprofit+=prices[i] - prices[i-1]
         
         return maxprofit
         
