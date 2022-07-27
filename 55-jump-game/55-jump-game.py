6a2a47bfa6be9bc1ed3d33b25bc0533d04ad08b0
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-07-26 23:38:07 -0400

    Time: 984 ms (21.09%), Space: 15.1 MB (96.67%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,30 +1,18 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        """
-        f(i) # maximum index to jump from i
         
-        f(i) = max(f(i-1),nums[i]+i)
-        
-        
-        
-        """
-        
-        if nums[0]==0 and len(nums)!=1:
+        if nums[0] == 0 and len(nums) != 1:
             return False
-        n = len(nums)   
-        DP=[-1]*len(nums)
         
         
+        DP = [0] *len(nums)
         DP[0] = nums[0]
-        for i in range(1,n-1):
+        
+        for i in range(1,len(nums)-1):
             DP[i] = max(DP[i-1],nums[i]+i)
             if DP[i] < i+1:
                 return False
             
-        
-        return DP[n-2] >= n-1
             
-        
-
-        
-        
+        return DP[len(nums)-2] >= len(nums)-1
+            
