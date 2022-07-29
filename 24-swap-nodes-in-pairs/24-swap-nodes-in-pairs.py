26013e43c01b5532eb60c67f289c10c862747550
Agrim Bhalla <60762418+mechtotech@users.noreply.github.com>
2022-07-28 23:21:20 -0400

    Time: 48 ms (51.45%), Space: 13.9 MB (64.14%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -10,26 +10,72 @@ class Solution:
             return None
         
         
-        #sentinel = ListNode("dummy",head)
+        sentinelo = ListNode("dummy")
+        tailo = sentinelo
         
-        prev = head
-        curr = head.next
-        step = 0
+        sentinele = ListNode("dummy")
+        taile = sentinele
+        
+        
+        index = 1
+        curr = head
+        prev = None
+        
         while curr:
-            if step%2 == 0:
-                currvalue = curr.val
-                prevvalue = prev.val
-                curr.val = prevvalue
-                prev.val = currvalue
+            succ= curr.next
+            if index%2 !=0:
+                tailo.next = curr
+                tailo = curr
+                tailo.next = None
+                curr = succ
                 
-            step+=1
-            prev = curr
-            curr= curr.next
+            else:
+                taile.next = curr
+                taile = curr
+                taile.next = None
+                curr = succ
+            index+=1
+        
+        sentinel = ListNode("dummy")
+        tail = sentinel
+                
+        curr2 = sentinelo.next
+        curr1 = sentinele.next
+        
+        while curr1 and curr2:
+            succ1 = curr1.next
+            tail.next = curr1 #s--->2
+            tail = curr1
+            curr1 = succ1
+            
+            succ2 = curr2.next
+            tail.next = curr2 #s--->2-->1
+            tail = curr2
+            curr2= succ2
             
         
-        return head
+        while curr2:      #s--->2--->1--->3
+                           #    new_head ==>2
+            tail.next = curr2
+            tail = curr2
+            curr2 = curr2.next
+        return sentinel.next
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+    
                 
-            
-            
+                
+                
+        
         
         
